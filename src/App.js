1e74d7f74cf6e914d4fdc6fb3aebbec211289fb9
Saumya <saumyashrimaludpr15@gmail.com>
2021-06-27 19:08:00 +0530

    Error handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import inputActionCreator from './Store/inputActionCreator';
@@ -7,187 +6,325 @@ import Dependent from './Components/Dependents';
 import SavedDependents from './Components/SavedDependents'
 import axios from 'axios';
 import { saveAs } from 'file-saver';
-import {BrowserRouter , Route , Switch , Link} from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+
 
 
 function App() {
 
 
+
   let StoreObj = useSelector((store) => store);
-
   let [dependentcount, setDependentCount] = useState(StoreObj.inputstate.dependents.length);
+
   let [addDependentDisable, setAddDependentDisable] = useState([]);
-
-
   const dispatch = useDispatch();
   const { setName, setAge, setDob, setGender, setPlace, setAdhaarno, setMobileno, setEmail, setDependent, deleteDependent } = bindActionCreators(inputActionCreator, dispatch);
+
+  let primaryErrInitialState = { adhaarnoerr: "", mobilenoErr: "" }
+
+  let [primaryErrState, setPrimaryErrState] = useState(primaryErrInitialState)
+
 
 
 
   let handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(StoreObj.inputstate);
+    setPrimaryErrState(primaryErrInitialState);
+    let adhaarcheck = StoreObj.inputstate.adhaarno.match(/^[0-9]{12,12}$/g)
+    let mobilenocheck = StoreObj.inputstate.mobileno.match(/^[0-9]{10,10}$/g)
+    if (adhaarcheck === null) {
 
-    axios.post('/create-pdf', StoreObj.inputstate)
-      .then(() => axios.get("/fetch-pdf", { responseType: 'blob' }))
-      .then(res => {
-        const pdfBlob = new Blob([res.data], { type: "application/pdf" })
-        saveAs(pdfBlob, "newpdf")
+      setPrimaryErrState((errState) => {
+
+        errState = { ...errState, adhaarno: "adhaar no should be of 12 digits" }
+
+        return errState;
+
       })
+
+    }
+    else if (mobilenocheck === null) {
+
+      setPrimaryErrState((errState) => {
+
+        errState = { ...errState, mobileno: "mobile no should be of 10 digits" }
+
+        return errState;
+
+      })
+
+    }
+    else {
+
+      console.log(StoreObj.inputstate);
+
+      axios.post('/create-pdf', StoreObj.inputstate)
+
+        .then(() => axios.get("/fetch-pdf", { responseType: 'blob' }))
+
+        .then(res => {
+
+          const pdfBlob = new Blob([res.data], { type: "application/pdf" })
+
+          saveAs(pdfBlob, "newpdf")
+
+        })
+
+    }
+
 
 
   }
+
 
 
   let handleGenderchange = (e, gender) => {
+
     e.preventDefault();
+
     console.log(gender)
+
     setGender(gender);
+
   }
+
+
 
   let handleDependentClick = (e) => {
+
     e.preventDefault();
-    if(dependentcount < 4){
+
+    if (dependentcount < 4) {
+
       setDependentCount(dependentcount + 1);
+
     }
-    else(
+
+    else (
+
       alert("only 4 dependents allowed")
+
     )
-    
-    
+
+
+
 
   }
 
+
+
   useEffect(() => {
+
     console.log("store obj after dependent add", StoreObj.inputstate);
-    if((dependentcount - StoreObj.inputstate.dependents.length) > 0){
+    if ((dependentcount - StoreObj.inputstate.dependents.length) > 0) {
       setAddDependentDisable(true);
     }
-    else{
+    else {
       setAddDependentDisable(false);
     }
   }, [StoreObj.inputstate.dependents, dependentcount])
 
+
+
   //update the dependent details
+
   // let editDependent = (dependentObj) => {
+
   //   setDependentCount(dependentcount + 1);
+
+
 
   // }
 
+
+
   //delete the dependent details
+
   let deleteSavedDependent = (e, dependentObj) => {
     e.preventDefault();
     deleteDependent(dependentObj);
     setDependentCount(dependentcount - 1);
-
   }
 
 
+
   return (
+
     <form className="w-75 mx-auto p-5 mt-3 mb-5" onSubmit={handleFormSubmit}>
       <h1>Covid Registration Form</h1>
-
       {/* input field for Name */}
+
       <input type="text" id="name" placeholder="Name..." className="form-control mb-3"
         value={StoreObj.inputstate.name}
         onChange={e => setName(e.target.value)}
         autoComplete="off"
         required
+
       />
+
 
 
 
       {/* input field for dob */}
+
       <input type="date" name="dob" placeholder="Date of birth..." id="dob" className="form-control mb-3"
+
         autoComplete="off"
         value={StoreObj.inputstate.dob}
         onChange={e => setDob(e.target.value)}
         required
+
       />
 
 
+
       {/* input field for age  */}
+
       <input type="number" name="age" placeholder="Age..." id="age" className="form-control mb-3"
         value={StoreObj.inputstate.age}
         autoComplete="off"
         onChange={e => setAge(e.target.value)}
         required
+
       />
+
 
 
       {/* radio button for gender */}
 
+
+
       <div className="d-flex mb-3">
         <div className="form-check me-3">
           <input type="radio" name="gender" id="male" className="form-check-input" value="male" onChange={(e) => handleGenderchange(e, e.target.value)} />
+
           <label htmlFor="male" className="form-check-label">Male</label>
+
         </div>
 
+
+
         <div className="form-check">
+
           <input type="radio" name="gender" id="female" className="form-check-input" value="female" onChange={(e) => handleGenderchange(e, e.target.value)} />
+
           <label htmlFor="female" className="form-check-label">Female</label>
+
         </div>
+
       </div>
 
 
 
 
+
       {/* input field for place/city */}
+
       <input type="text" id="city" placeholder="city..." className="form-control mb-3"
+
         value={StoreObj.inputstate.place}
+
         onChange={e => setPlace(e.target.value)}
+
         autoComplete="off"
+
         required
+
       />
 
+
+
       {/* input field for adhaar no */}
+
       <input type="text" id="adhaarno" placeholder="adhaar..." className="form-control mb-3"
+
         value={StoreObj.inputstate.adhaarno}
+
         onChange={e => setAdhaarno(e.target.value)}
+
         autoComplete="off"
+
         required
+
       />
 
+      {primaryErrState.adhaarno && <p className="text-danger">{primaryErrState.adhaarno}</p>}
+
+
+
       {/* input field for adhaar no */}
+
       <input type="text" id="mobileno" placeholder="Mobile no..." className="form-control mb-3"
+
         value={StoreObj.inputstate.mobileno}
+
         onChange={e => setMobileno(e.target.value)}
+
         autoComplete="off"
+
         required
+
       />
 
+      {primaryErrState.mobileno && <p className="text-danger">{primaryErrState.mobileno}</p>}
+
+
+
       {/* input field for adhaar no */}
+
       <input type="email" id="email" placeholder="Email..." className="form-control mb-3"
+
         value={StoreObj.inputstate.email}
+
         onChange={e => setEmail(e.target.value)}
+
         autoComplete="off"
+
         required
+
       />
+
 
 
 
       <p className="text-danger mt-4">Note : only 4 dependents can be added</p>
+
       {/* button to add dependent */}
+
+
 
       <button className="p-2 btn btn-primary d-block m-1" disabled={addDependentDisable} onClick={handleDependentClick}>Add Dependent</button>
 
 
 
-      {
-        (dependentcount - StoreObj.inputstate.dependents.length) > 0 && <Dependent dependentno={dependentcount} setDependent={setDependent} />
-      }
 
       {
-        StoreObj.inputstate.dependents.length > 0 && <SavedDependents dependents={StoreObj.inputstate.dependents} deleteSavedDependent={deleteSavedDependent} />
+
+        (dependentcount - StoreObj.inputstate.dependents.length) > 0 && <Dependent dependentno={dependentcount} setDependent={setDependent} />
+
       }
+
+
+
+      {
+
+        StoreObj.inputstate.dependents.length > 0 && <SavedDependents dependents={StoreObj.inputstate.dependents} deleteSavedDependent={deleteSavedDependent} />
+
+      }
+
 
 
       <button type="submit" className="btn btn-success d-block text-center mt-4 me-1 mb-3">Submit</button>
 
 
+
     </form>
+
   );
+
 }
+
 
 
 export default App;
--- src/Components/Dependents.js
+++ src/Components/Dependents.js
@@ -1,123 +1,375 @@
 import {useState,useEffect} from 'react';
+
 import { useSelector, useDispatch } from 'react-redux';
+
 import dependentActionCreator from '../Store/dependentActionCreator';
+
 import { bindActionCreators } from 'redux';
+
+ 
 
 function Dependents(props) {
 
+ 
+
     let StoreObj = useSelector((store) => store);
+
     const dispatch = useDispatch();
 
-    let [dependentErrState , setDependentErrState] = useState({name:"",age:"",dob:"",gender:"",place:"",relation:"",adhaarno:""})
+    let initialerrstate = {name:"",age:"",dob:"",gender:"",place:"",relation:"",adhaarno:""};
+
+ 
+
+    let [dependentErrState , setDependentErrState] = useState(initialerrstate);
+
+ 
 
     const {setdName, setdAge, setdDob, setdGender, setdPlace, setdAdhaarno, setRelation ,Reset,setdno} = bindActionCreators(dependentActionCreator, dispatch);
 
+ 
+
     let handleGenderchange = (e, gender) => {
+
         e.preventDefault();
+
         console.log(gender)
+
         setdGender(gender);
+
     }
+
     let relations = ["father", "mother", "spouse", "son", "daughter"];
+
     useEffect(()=>{
+
         setdno(props.dependentno);
+
     },[])
+
+ 
 
     
 
+ 
+
     let handleAddDependent = (e) => {
+
         e.preventDefault();
-        console.log("store obj",StoreObj.dependentState);
-        props.setDependent(StoreObj.dependentState);
-        Reset();
+
+ 
+
+        console.log("store obj inside add dependent = ", StoreObj.dependentState);
+
+ 
+
+        setDependentErrState(initialerrstate);
+
+ 
+
+        // check adhaar for dependent for should be 12 digits
+
+ 
+
+        let adhaarcheck =  StoreObj.dependentState.adhaarno.match(/^[0-9]{12,12}$/g)
+
+ 
+
+        if(StoreObj.dependentState.name === ""){
+
+            setDependentErrState((errState) => {
+
+                errState = {...errState,name:"name required"}
+
+                return errState;
+
+            })
+
+        }
+
+        else if(StoreObj.dependentState.adhaarno === ""){
+
+            setDependentErrState((errState) => {
+
+                errState = {...errState,adhaarno:"Adhaar no required"}
+
+                return errState;
+
+            })
+
+        }
+
+        else if(adhaarcheck === null){
+
+            setDependentErrState((errState) => {
+
+                errState = {...errState,adhaarno:"should be 12 digits"}
+
+                return errState;
+
+            })
+
+        }
+
+        else if(StoreObj.dependentState.relation === ""){
+
+            setDependentErrState((errState) => {
+
+                errState = {...errState,relation:"choose relation"}
+
+                return errState;
+
+            })
+
+        }
+
+ 
+
+        else if(StoreObj.dependentState.age === ""){
+
+            setDependentErrState((errState) => {
+
+                errState = {...errState , age:"age is required"}
+
+                return errState;
+
+            })
+
+        }
+
+ 
+
+        else if(StoreObj.dependentState.dob === ""){
+
+            setDependentErrState((errState) => {
+
+                errState = {...errState , age:"DOB is required"}
+
+                return errState;
+
+            })
+
+        }
+
+ 
+
+        
+
+        else if(StoreObj.dependentState.gender === ""){
+
+            setDependentErrState((errState) => {
+
+                errState = {...errState , gender: "gender is required"}
+
+                return errState;
+
+            })
+
+        }
+
+ 
+
+        else if(StoreObj.dependentState.place === ""){
+
+            setDependentErrState((errState) => {
+
+                errState = {...errState , place: "place is required"}
+
+                return errState;
+
+            })
+
+        }
+
+ 
+
+        else{
+
+            console.log("store obj",StoreObj.dependentState);
+
+            props.setDependent(StoreObj.dependentState);
+
+            Reset();
+
+        }
+
+       
+
+ 
 
     }
 
+ 
 
     return (
+
         <form className="w-100 mx-auto p-5 mt-3 mb-5">
+
             <h5>Dependents {props.dependentno}</h5>
 
+ 
+
             <select className="form-select mb-3" id="relation"
+
                 value={StoreObj.dependentState.relation}
+
                 onChange={e => setRelation(e.target.value)}
+
                 required>
+
                 {relations.map((rel, key) => {
+
                     return (
+
                         <option key={key} value={rel}>{rel}</option>
+
                     );
+
                 })}
+
             </select>
 
+            {dependentErrState.relation && <p className="text-danger">{dependentErrState.relation}</p> }
+
+ 
+
             {/* input field for Name */}
+
             <input type="text" id="name" placeholder="Name..." className="form-control mb-3"
+
                 value={StoreObj.dependentState.name}
+
                 onChange={e => setdName(e.target.value)}
+
                 autoComplete="off"
+
                 required
+
             />
+
+            {dependentErrState.name && <p className="text-danger">{dependentErrState.name}</p> }
+
 
 
 
             {/* input field for dob */}
+
             <input type="date" name="dob" placeholder="Date of birth..." id="dob" className="form-control mb-3"
+
                 autoComplete="off"
+
                 value={StoreObj.dependentState.dob}
+
                 onChange={e => setdDob(e.target.value)}
+
                 required
+
             />
 
+            {dependentErrState.dob && <p className="text-danger">{dependentErrState.dob}</p> }
+
+ 
 
             {/* input field for age  */}
+
             <input type="number" name="age" placeholder="Age..." id="age" className="form-control mb-3"
+
                 value={StoreObj.dependentState.age}
+
                 autoComplete="off"
+
                 onChange={e => setdAge(e.target.value)}
+
                 required
+
             />
 
+            {dependentErrState.age && <p className="text-danger">{dependentErrState.age}</p> }
+
+ 
 
             {/* radio button for gender */}
 
+ 
+
             <div className="d-flex mb-3">
+
                 <div className="form-check me-3">
+
                     <input type="radio" name="gender" id="male" className="form-check-input" value="male" onChange={(e) => handleGenderchange(e, e.target.value)} />
+
                     <label htmlFor="male" className="form-check-label">Male</label>
+
                 </div>
 
+ 
+
                 <div className="form-check">
+
                     <input type="radio" name="gender" id="female" className="form-check-input" value="female" onChange={(e) => handleGenderchange(e, e.target.value)} />
+
                     <label htmlFor="female" className="form-check-label">Female</label>
+
                 </div>
+
             </div>
+
+            {dependentErrState.gender && <p className="text-danger">{dependentErrState.gender}</p> }
 
 
 
 
             {/* input field for place/city */}
+
             <input type="text" id="city" placeholder="city..." className="form-control mb-3"
+
                 value={StoreObj.dependentState.place}
+
                 onChange={e => setdPlace(e.target.value)}
+
                 autoComplete="off"
+
                 required
+
             />
+
+            {dependentErrState.place && <p className="text-danger">{dependentErrState.place}</p> }
+
+ 
 
             {/* input field for adhaar no */}
+
             <input type="text" id="adhaarno" placeholder="adhaar..." className="form-control mb-3"
+
                 value={StoreObj.dependentState.adhaarno}
+
                 onChange={e => setdAdhaarno(e.target.value)}
+
                 autoComplete="off"
+
                 required
+
             />
 
+            {dependentErrState.adhaarno && <p className="text-danger">{dependentErrState.adhaarno}</p> }
+
+ 
 
             <button className="btn btn-primary" onClick={handleAddDependent}>Add</button>
 
+ 
+
             <hr className="solid"/>
 
+ 
 
         </form>
+
     );
 
+ 
+
 }
+
+ 
 
 export default Dependents
